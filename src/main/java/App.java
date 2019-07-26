a8f0199b590d006c98f4ba370f8c17bf45ac4687
it179018321 <53336050+it179018321@users.noreply.github.com>
2019-07-26 18:28:54 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello  我来了");
-        System.out.println("hello  我来了");
-        System.out.println("hello  我来了");
-        System.out.println("hello  我来了");
-        System.out.println("hello  我来了");
+        System.out.println("hello  我来了1111");
+        System.out.println("hello  我来了2222");
+      
     }
 }
