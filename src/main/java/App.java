32d9f3206623825c249049ee0734861b657edddc
传智专修学院 <czxy@163.com>
2019-07-26 18:21:45 +0800

    Git提交测试2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("hello  我来了");
+        System.out.println("hello  我来了");
+        System.out.println("hello  我来了");
+        System.out.println("hello  我来了");
+        System.out.println("hello  我来了");
     }
 }
